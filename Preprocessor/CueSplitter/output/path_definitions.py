2b36a43d7397191f0e6147201f7a77c6cee2aaa2
sqz269 <yin2690855381@gmail.com>
2023-12-21 13:45:43 -0500

    Added path definitions

---
+++ Preprocessor/CueSplitter/output/path_definitions.py
@@ -0,0 +1,4 @@
+CUE_SCANNER_OUTPUT_NAME = "potential.json"
+CUE_DESIGNATER_OUTPUT_NAME = "designated.json"
+CUE_DESIGNATER_USER_PAIR_CACHE_NAME = "user_pair_cache.json"
+CUE_SPLITTER_OUTPUT_NAME = "split.json"
