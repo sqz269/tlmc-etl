f66b9d43d7f48b0a0ab88df84975a2dbdafd1dcd
sqz269 <yin2690855381@gmail.com>
2024-04-18 08:32:48 -0500

    Changed hash to streaming hash to avoid OOM

--- Preprocessor/Extract/unextracted_snapshot.py
+++ Preprocessor/Extract/unextracted_snapshot.py
@@ -3,11 +3,14 @@ import os
 from typing import List
 import xxhash
 
-from Shared.utils import get_self_output_path
+from Shared.utils import get_output_path
 
-from Preprocessor.Extract.output.path_definitions import UNEXTRACTED_RAR_SNAPSHOT_OUTPUT_NAME
+import Preprocessor.Extract.output.path_definitions as ExtractOutputPaths
 
-output_path = get_self_output_path(__file__, UNEXTRACTED_RAR_SNAPSHOT_OUTPUT_NAME)
+output_path = get_output_path(
+    ExtractOutputPaths, ExtractOutputPaths.UNEXTRACTED_RAR_SNAPSHOT_OUTPUT_NAME
+)
+
 
 def generate_rar_list(root: str):
     rar_files = []
@@ -22,31 +25,49 @@ def generate_rar_list(root: str):
 def filter_rar_list_by_completed(filelist: List[str]):
     # File will be structued as {filename: {hash: str, size: int}}
     # each line in the output file will be a separate JSON object
-    with open(output_path, "r") as f:
+    with open(output_path, "r", encoding="utf-8") as f:
         completed_files = [json.loads(line) for line in f.readlines()]
-        completed_files = {file["filename"]: file for file in completed_files}
+        completed_files = {list(file.keys())[0] for file in completed_files}
 
-    return [file for file in filelist if os.path.basename(file) not in completed_files]
+    return [file for file in filelist if file not in completed_files]
+
 
 def generate_rar_snapshot(filelist: List[str]):
     for idx, file in enumerate(filelist):
         with open(file, "rb") as f:
             print(f"[{idx}/{len(filelist)}] Hashing {file}", end="\r")
-            hash = xxhash.xxh3_128_hexdigest(f.read())
+
+            # Stream hash the file
+            hash = xxhash.xxh64()
+            while True:
+                data = f.read(4096)
+                if not data:
+                    break
+                hash.update(data)
+
             size = os.path.getsize(file)
 
             obj = {
                 file: {
-                    "hash": hash,
+                    "hash": hash.hexdigest(),
                     "size": size,
                 }
             }
 
-            with open(output_path, "a") as f:
-                f.write(json.dumps(obj) + "\n")
+            with open(output_path, "a", encoding="utf-8") as f:
+                f.write(json.dumps(obj, ensure_ascii=False) + "\n")
+
 
 if __name__ == "__main__":
     tlmc_root = input("Enter TLMC root path: ")
+
+    if not os.path.exists(tlmc_root):
+        print("Invalid path")
+        exit(1)
+
+    if not os.path.isfile(output_path):
+        with open(output_path, "w", encoding="utf-8") as f:
+            pass
 
     print("Generating RAR file list...")
     rar_files = generate_rar_list(tlmc_root)
