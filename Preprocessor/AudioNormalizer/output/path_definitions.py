e094459e0ffac0f4ff2f3027bf179c64280c8ae8
sqz269 <yin2690855381@gmail.com>
2024-04-19 16:08:28 -0500

    Added stage 2 norm, and detect sample fmt

--- Preprocessor/AudioNormalizer/output/path_definitions.py
+++ Preprocessor/AudioNormalizer/output/path_definitions.py
@@ -5,6 +5,9 @@ NORMALIZE_FIRST_PASS_DETECT_OUTPUT_FILELIST_NAME = (
 NORMALIZE_FIRST_PASS_DETECT_FAILED_NAME = "normalize.firstpass_detect.failed.log"
 NORMALIZE_FIRST_PASS_DETECT_JOURNAL_NAME = "normalize.firstpass_detect.journal.log"
 
+NORMALIZE_SECOND_PASS_CONVERT_WORKLIST_PATH = (
+    "normalize.secondpass_convert.worklist.json"
+)
 NORMALIZE_SECOND_PASS_CONVERT_COMPLETED_NAME = (
     "normalize.secondpass_convert.processed.json"
 )
