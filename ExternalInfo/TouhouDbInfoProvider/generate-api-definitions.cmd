44cf5ca7b9dcf352172da1ab631a35cdb780e109
sqz269 <yin2690855381@gmail.com>
2024-06-16 22:27:49 -0400

    Added ground work for touhoudb integration

---
+++ ExternalInfo/TouhouDbInfoProvider/generate-api-definitions.cmd
@@ -0,0 +1,1 @@
+openapi-generator-cli.cmd generate -i https://touhoudb.com/swagger/v1/swagger.json -g python -o touhoudb_api_client --additional-properties=packageName=touhoudb_api_client --additional-properties=projectName=touhoudb-api-client
