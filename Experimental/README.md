0010b1d7137af9fa41fc51050e77bf8141b16276
sqz269 <yin2690855381@gmail.com>
2025-11-03 21:20:09 -0500

    Experimental: add README, implement batch FLAC embedding pipeline, update deps and lockfile

---
+++ Experimental/README.md
@@ -0,0 +1,6 @@
+# Experimental Code for Recommender Systems
+
+This folder contains code for recommender experiments
+
+## Blog Entries
+### [Shining Needle in a 4TB Haystack: Recommending Music from the Touhou Lossless Music Collection (Pt. 1)](https://blog.sqz269.me/2025/11/03/tlmc-rec-01.html)
--- Experimental/main.py
+++ Experimental/main.py
@@ -1,5 +1,8 @@
 # ---- put THIS at the very top of mert_batch.py ----
+from genericpath import isdir
+from importlib.metadata import files
 import os, sys
+from typing import Dict, List
 
 try:
   import dotenv
@@ -31,6 +34,7 @@ if ffmpeg_bin:
 
 import numpy as np
 import torch
+import requests
 from loader import load_flac, load_m3u8_playlist_remote, ChunkingConfig
 import mert
 from utils import utils
@@ -42,40 +46,56 @@ chunking_config = ChunkingConfig(
   overlap_size=5,
 )
 
+def get_flac_list(dir_path: str) -> Dict[str, List[str]]:
+  # genre and list of songs
+  flac_files: Dict[str, List[str]] = {}
+  # first level dir is genre info
+  for item in os.listdir(dir_path):
+    path = os.path.join(dir_path, item)
+    if not os.path.isdir(path):
+      continue
+
+    flac_files[item] = []
+    for fp, _, files in os.walk(path):
+      for f in files:
+        if f.lower().endswith(".flac"):
+          full_path = os.path.join(fp, f)
+          if ("[ignore]" in full_path.lower()):
+            continue
+          flac_files[item].append(full_path)
+
+  return flac_files
+
 def main():
-  audio_chunks_flac = load_flac("data/(01) [二羽凛奈，朔間咲] 明日世界をなくしても.flac", chunking_config)
-  audio_chunks_hls = load_m3u8_playlist_remote("https://staging-api.marisad.me/api/asset/track/b7963120-4ab6-4c04-b584-bb76396bb552/hls/128k/playlist.m3u8", chunking_config)
+  flac_list = get_flac_list("data/")
 
-  embeddings_flac = mert.embed_waveforms_batched(
-    chunks=audio_chunks_flac.chunks,
-    sr_in=audio_chunks_flac.sample_rate,
-    layer_mix="last4",
-    batch_size=3,
-    num_workers=1,  # parallel CPU preprocessing
-    pin_memory=True
-  )
+  for genre, files in flac_list.items():
+    for i in files:
+      print("Processing track ID:", i)
+      audio_chunks_hls = load_flac(
+        fp=i,
+        chunking_config=chunking_config,
+      )
 
-  print("Embeddings FLAC shape:", embeddings_flac.shape)
+      embeddings_hls = mert.embed_waveforms_batched(
+        chunks=audio_chunks_hls.chunks,
+        sr_in=audio_chunks_hls.sample_rate,
+        layer_mix="last4",
+        batch_size=3,
+        num_workers=1,  # parallel CPU preprocessing
+        pin_memory=True
+      )
 
-  embeddings_hls = mert.embed_waveforms_batched(
-    chunks=audio_chunks_hls.chunks,
-    sr_in=audio_chunks_hls.sample_rate,
-    layer_mix="last4",
-    batch_size=3,
-    num_workers=1,  # parallel CPU preprocessing
-    pin_memory=True
-  )
+      print("Embeddings HLS shape:", embeddings_hls.shape)
 
-  print("Embeddings HLS shape:", embeddings_hls.shape)
+      hls_pooled = utils.pool(tensor=embeddings_hls, mode="mean+max");
 
-  flac_pooled = utils.pool(embeddings_flac, mode="mean");
-  hls_pooled = utils.pool(embeddings_hls, mode="mean");
+      print("Pooled HLS shape:", hls_pooled.shape)
 
-  print("Pooled FLAC shape:", flac_pooled.shape)
-  print("Pooled HLS shape:", hls_pooled.shape)
+      # get file name
+      filename = os.path.splitext(os.path.basename(i))[0]
 
-  utils.save_tensor(flac_pooled, "embeddings/pooled_flac.pt")
-  utils.save_tensor(hls_pooled, "embeddings/pooled_hls.pt")
+      utils.save_tensor(hls_pooled, f"embeddings/[{genre}] - {filename}.pt")
 
 if __name__ == "__main__":
   main()
